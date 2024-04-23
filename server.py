3bff0ea2997d51a57afd20accf59bd495f70ebf7
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-04-23 17:07:31 +0100

    Added and started working with HTMX - Added htmx cdn to header in _macros.html - Created static folder - Added styles.css to static folder - Added link to styles.css in head() inside _macros.html - Created _partials folder - Added edit.html with card for editing posts inside _partials - Created edit_post inside server.py - Added hx-get to edit post button in posts.html - Added hx-target=this and hx-swap=outerHTML to outer card div in posts.html

--- server.py
+++ server.py
@@ -13,3 +13,9 @@ def home():
 def posts():
     posts = sample_db.get_posts()
     return render_template("posts.html", posts=posts)
+
+
+@app.route("/posts/edit/<int:post_id>")
+def edit_post(post_id):
+    post = sample_db.get_post(post_id)
+    return render_template("_partials/edit.html", post=post)
