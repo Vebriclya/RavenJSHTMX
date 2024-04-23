3ab449326a54ffea3d5432dd99e5af0fb392cc1c
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-04-23 13:06:39 +0100

    Created homepage.html and posts.html - Adjusted hello_world() in server.py to be home - Adjusted index.html to use templatnig - That should say templating and now I can't edit it, fml - Created and populated homepage.html and posts.html - Adjusted the links in the navbar in _macros.html to use url_for

--- server.py
+++ server.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def home():
+    return render_template("homepage.html")
+
+
+@app.route("/posts")
+def posts():
+    return render_template("posts.html")
