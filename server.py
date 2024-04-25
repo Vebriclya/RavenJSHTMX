1f8136ec284ff99993e40f02bdbd9b1ad0fc8b86
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2024-04-25 11:50:36 +0100

    Added functionality to edit posts - Imported request to server.py - Modified edit_post() inside server.py to handle GET and PUT methods - Added hx-target and hx-swap to outer div inside edit.html - Added hx-put to submit button within edit.html - Added postcard.html to _partials folder, populated with card from posts.html - Removed card from posts.html, and replaced with div using hx-get to fetch show_post() from server.py - Added show_post() function to server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from db import sample_db
 
 app = Flask(__name__)
@@ -15,7 +15,19 @@ def posts():
     return render_template("posts.html", posts=posts)
 
 
-@app.route("/posts/edit/<int:post_id>")
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    post = sample_db.get_post(post_id)
+    return render_template("_partials/postcard.html", post=post)
+
+
+@app.route("/posts/edit/<int:post_id>", methods=["GET", "PUT"])
 def edit_post(post_id):
+    if request.method == "PUT":
+        title = request.form.get("title")
+        content = request.form.get("content")
+        sample_db.update_post(post_id, title=title, content=content)
+        return show_post(post_id)
+    # Default GET behaviour
     post = sample_db.get_post(post_id)
     return render_template("_partials/edit.html", post=post)
